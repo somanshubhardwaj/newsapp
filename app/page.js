16fc6476d0efbdfad43b27e78a53ba5097cb07a7
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-01-31 16:58:45 +0530

    Update UI styling for NEWS app

--- app/page.js
+++ app/page.js
@@ -1,39 +1,150 @@
 import Image from "next/image";
 async function fetchNews() {
   const res = await fetch(
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=15618522b32b4694bafd42d58741a875"
+    "https://newsapi.org/v2/top-headlines?country=in&apiKey=15618522b32b4694bafd42d58741a875"
   );
   const data = await res.json();
   return data;
 }
 export default async function Home() {
   const news = await fetchNews();
+  console.log(news);
   return (
     <>
       <navbar className="bg-gray-800">
-        
+        <header class="text-gray-600 body-font">
+          <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto"></nav>
+            <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+              <span class="ml-3 text-xl">NEWS APP</span>
+            </a>
+            <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0"></div>
+          </div>
+        </header>
       </navbar>
       <div className="container mx-auto">
-        <h1 className="text-2xl font-bold">News App</h1>
-        <p>
-          News App is a web application that allows users to view the latest
-          news from various sources.
-        </p>
-        <h2 className="text-xl font-bold mt-4">News</h2>
-        <ul className="mt-2">
-          {news.articles.map((article, index) => (
-            <li key={index} className="my-4">
-              <h3 className="text-lg font-bold">{article.title}</h3>
-              <p>{article.description}</p>
-              <img src={article.urlToImage} alt="article image" className="my-2" />
-              <a href={article.url}>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded mt-2">
-                  Read More
-                </button>
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap -m-4">
+              {news.articles.map((article, index) => (
+                <div class="p-4 md:w-1/3">
+                  <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                    <img
+                      class="lg:h-48 md:h-36 w-full object-cover object-center"
+                      src={article.urlToImage}
+                      alt="blog"
+                    />
+                    <div class="p-6">
+                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                        {article.author}
+                      </h2>
+                      <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                        {article.title}
+                      </h1>
+                      <p class="leading-relaxed mb-3">{article.description}</p>
+                      <div class="flex items-center flex-wrap ">
+                        <a
+                          class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                          href={article.url}
+                          target="_blank"
+                        >
+                          Learn More
+                          <svg
+                            class="w-4 h-4 ml-2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          >
+                            <path d="M5 12h14"></path>
+                            <path d="M12 5l7 7-7 7"></path>
+                          </svg>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <footer>
+          <footer class="text-gray-600 body-font">
+            <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+              <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                <span class="ml-3 text-xl">NEWS APP</span>
               </a>
-            </li>
-          ))}
-        </ul>
+
+              <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                <a class="text-gray-500">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-3 text-gray-500">
+                  <svg
+                    fill="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-3 text-gray-500">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <rect
+                      width="20"
+                      height="20"
+                      x="2"
+                      y="2"
+                      rx="5"
+                      ry="5"
+                    ></rect>
+                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                  </svg>
+                </a>
+                <a class="ml-3 text-gray-500">
+                  <svg
+                    fill="currentColor"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="0"
+                    class="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="none"
+                      d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                    ></path>
+                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                  </svg>
+                </a>
+              </span>
+            </div>
+          </footer>
+        </footer>
       </div>
     </>
   );
